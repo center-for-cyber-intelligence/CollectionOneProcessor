684fd81ba9941c739ea86394159e21c571ab94bf
Chris Cooley <chris.cooley@centerforcyberintelligence.org>
2019-02-26 21:03:58 -0500

    Addressed Issue #5

--- mainTest.py
+++ mainTest.py
@@ -1,10 +1,12 @@
 import os
 import mysql.connector
+from datetime import datetime
 from mysql.connector import Error
 from dataProcessor import dataProcessor
 
 def main():
 
+    script_start_time = datetime.now()
     # Throw this script in the directory you want or use the below os.chdir to specify the directory you want to look
     # in (just make sure to comment line 148 out if you move the script to your working directory
     os.chdir('./Testing/')
@@ -16,10 +18,22 @@ def main():
     # Pass the current working directory to function to get a list of files to process and assign it to a variable
     filesToProcess = getFiles(currentWD)
 
+    fileCount = 0
+    for file in filesToProcess:
+        fileCount += 1
+    num_lines = 0
+
     # Useful to see if you are looking at the right directory
-    print "\n[****] Here are the files I found for processing [****]\n\n", filesToProcess
+    print "\n[*] Here are the files I found for processing...\n\n", filesToProcess
 
     for workingFile in filesToProcess:
+        fileProcessingStart = datetime.now()
+        filelines = 0
+        with open(workingFile, 'r') as f:
+            for line in f:
+                num_lines += 1
+                filelines += 1
+            f.close()
 
         # Get the file folder and file name for each file processed - assign to a variable to pass to on to the
         # processing function
@@ -28,23 +42,33 @@ def main():
         file_name = str_split[len(str_split) - 1]
 
         # Pass the working file to the dataProcessor class
-        print "\n[****] Processing file: ", workingFile
+        print "\n[*] Processing %s lines in file %s" % (filelines, workingFile)
         dataP.processFile(workingFile, file_folder, file_name)
+        fileProcessingEnd = datetime.now()
+        fileProcessingElapsed = fileProcessingEnd - fileProcessingStart
+        print "[*] Finished Processing File - Elapsed Time: ", fileProcessingElapsed
+        dataP.processFile(workingFile, file_folder, file_name)
+
+    script_end_time = datetime.now()
+    script_elapsed_time = script_end_time - script_start_time
+    print "[*] Analyzed %s files and %s total lines!" % (fileCount, num_lines)
+    print "[*] Total Elapsed Time: ", script_elapsed_time
 
 
 def getFiles(currentWD):
+    fileCount = 0
     filesToRead = []
     for dirname, dirnames, filenames in os.walk(currentWD):
         for filename in filenames:
-
             # ignore some files we don't care about
-            excludeList = ['.DS_Store', '._']
-
+            excludeList = ['.DS_Store', '._', 'dataProcessor.py', 'dataProcessor.pyc', 'FoundData.txt', 'mainTest.py']
             if any(item in filename for item in excludeList):
                 pass
             else:
                 filePath = os.path.join(dirname, filename)
                 filesToRead.append(filePath)
+                fileCount = fileCount + 1
+    print "\n[!] Preparing to analyze %s files...\n" % fileCount
     return filesToRead
 
 def dbConnection():
@@ -67,6 +91,7 @@ def dbConnection():
 
     except Error as e:
         print "[!] Error while connecting to MySQL - ", e
+        exit()
 
     return connection
 
